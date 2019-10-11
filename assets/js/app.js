6eef274b21fafd5079e8ed892b589794336f4ff7
Heggy Castaneda <heggy_castaneda@gap.com>
2019-10-10 21:42:41 -0700

    add all

---
+++ assets/js/app.js
@@ -0,0 +1,20 @@
+// message in the console
+console.log("Hi there, welcome to the JavaScript console!");
+
+// initialize the application
+var app = new Object();
+
+app.shakePokeball = function shakePokeball() {
+  // code BELOW this line for challenge #1 -->
+  
+} // <-- code ABOVE this line for challenge #1
+
+app.setName = function setName() {
+  // code BELOW this line for challenge #2 -->
+
+} // <-- code ABOVE this line for challenge #2
+
+app.setWebsite = function setWebsite() {
+  // code BELOW this line for challenge #3 & bonus -->
+
+} // <-- code ABOVE this line for challenge #3 & bonus
